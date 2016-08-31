f58c5e24d241265ae42284613ed174445a91fadd
James Lemire <webdev.jameslemire@gmail.com>
2016-08-31 14:28:30 -0600

    Add img folder, connect alderaan component

--- Projects/React-Nasa/public/app/reactApp.js
+++ Projects/React-Nasa/public/app/reactApp.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router';
+import Alderaan from './components/alderaan.js';
+import Character from './components/character.js';
 
 var App = React.createClass({
-
   render() {
     return (
       <div>
-        <p>Hello</p>
+        <p>Starwars API - Routes</p>
+        <Router>
+
+          <Route path="alderaan" component="Alderaan">
+            <Route path="character" component="Character"></Route>
+          </Route>
+
+        </Router>
       </div>
     )
   }
-
 });
 
 ReactDOM.render(<App />, document.getElementById('app'));
